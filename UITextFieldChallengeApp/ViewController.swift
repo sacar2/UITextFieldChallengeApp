0f02c90d5c60e99cba95df62af4b450290ed8bb6
Selin Acar <sacar2@uwo.ca>
2016-07-06 22:58:22 -0400

    Initial Commit

---
+++ UITextFieldChallengeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITextFieldChallengeApp
+//
+//  Created by selin acar on 2016-07-06.
+//  Copyright © 2016 selin acar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ UITextFieldChallengeAppTests/UITextFieldChallengeAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  UITextFieldChallengeAppTests.swift
+//  UITextFieldChallengeAppTests
+//
+//  Created by selin acar on 2016-07-06.
+//  Copyright © 2016 selin acar. All rights reserved.
+//
+
+import XCTest
+@testable import UITextFieldChallengeApp
+
+class UITextFieldChallengeAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
